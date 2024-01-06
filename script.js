f12e6b3faced028ecbdef798c11b4408809e79ec
pavanhub-47 <140827035+pavanhub-47@users.noreply.github.com>
2024-01-06 20:20:44 +0530

    animation

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var video = document.querySelector(".video-con");
+var videoCon = document.querySelector(".video-con");
 var play = document.querySelector(".play");
 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function videoAmination (){
-    video.addEventListener("mouseenter",function (dets){
+    videoCon.addEventListener("mouseenter",function (dets){
         gsap.to(play,{
             opacity:1,
             scale:1,
+            display:"block"
         })
     })
-    video.addEventListener("mouseleave",function (){
+    videoCon.addEventListener("mouseleave",function (){
         gsap.to(play,{
             opacity:0,
-            scale:0
+            scale:0,
+            display:"none"
         })
     })
-    video.addEventListener("mousemove",function (dets){
+    videoCon.addEventListener("mousemove",function (dets){
         gsap.to(play,{
             left:dets.x-40,
             top:dets.y-40
@@ -32,10 +39,12 @@ function loadingAnimation(){
         stagger:.5
     })
     gsap.from(".page1 .video-con",{
-        opacity:0.9,
-        scale:0,
-        delay:1,
+        opacity:0,
+        scale:.7,
+        delay:1.4,
         duraton:.4
     })
 }
 loadingAnimation();
+
+
