58c0de88e2062125f508a96a0053af5fe3d4328f
pavanhub-47 <140827035+pavanhub-47@users.noreply.github.com>
2024-01-07 22:34:02 +0530

    final

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 var videoCon = document.querySelector(".video-con");
 var play = document.querySelector(".play");
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+  
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+  
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true,
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+  
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform
+        ? "transform"
+        : "fixed",
+    });
+  
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+}
+locomotiveAnimation();
 
 function videoAmination (){
     videoCon.addEventListener("mouseenter",function (dets){
@@ -48,3 +85,49 @@ function loadingAnimation(){
 loadingAnimation();
 
 
+document.addEventListener("mousemove",function(dets){
+    gsap.to(".cursor",{
+       left: dets.x,
+       top: dets.y,      
+    })
+})
+document.querySelectorAll(".child")
+.forEach(function(elem,index){
+    let arr = ["#F7F2EC","#ECECEC","#EDE6E6","#D6E0E0"]
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(".cursor",{
+            transform: 'translate(-50%,-50%) scale(1)',
+            backgroundColor: arr[index],
+         })
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(".cursor",{
+             transform: 'translate(-50%,-50%) scale(0)'
+         })
+    })
+})
+
+
+function navAnimation(){
+    gsap.to(".nav-1>svg",{
+        transform: "translateY(-100%)",
+        scrollTrigger:{
+            scroller: "#main",
+            trigger:".nav-1>svg",
+            start : "top top",
+            scrub:true,
+        },  
+    })
+
+    gsap.to(".nav-2>a",{
+        opacity:"0",
+        transform: "translateY(-133%)",
+        scrollTrigger:{
+            scroller: "#main",
+            trigger:".nav-2>a",
+            start : "top  30%",
+            scrub:2
+        },
+    })
+}
+navAnimation();
